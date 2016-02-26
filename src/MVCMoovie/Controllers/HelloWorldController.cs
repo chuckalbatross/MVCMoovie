fa717603ad246e005b48d977d25462746b196aa6
chuckalbatross <james.bonner@schneider-electric.com>
2016-02-26 12:56:59 -0600

    Initial Commit
    Junk

---
+++ src/MVCMoovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCMoovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /<controller>/Welcome
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
